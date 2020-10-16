54eedc505c3e09a5f61fc770eb2aa06133ea1ee6
Kartik Mahajan <kartikmahajan96@gmail.com>
2020-10-17 01:34:47 +0530

    commit

---
+++ JS/Todo.js
@@ -0,0 +1,75 @@
+var ul = document.getElementById("list");
+var li;
+let div = document.querySelector(".controls");
+
+var addButton = document.getElementById("add");
+addButton.addEventListener("click",addItem)
+
+var removeButton = document.getElementById("remove");
+removeButton.addEventListener("click",removeItem)
+
+var removeAllButton = document.getElementById("removeAll");
+removeAllButton.addEventListener("click",removeAll)
+
+function addItem(){
+    var enteryBar = document.getElementById("input");
+    var item = enteryBar.value;
+    var textNode = document.createTextNode(item);
+    
+    if(item === ""){
+        var pTag = document.createElement("p") //Adding P tag
+        var text = document.createTextNode("Todo cannot be Null") // creating text 
+        pTag.appendChild(text); // merging P tag and text
+        document.querySelector("div").appendChild(pTag); // where to be appended
+        div.insertAdjacentElement("afterend",pTag) // div is variable mentioned at top
+        document.querySelector("p").style.color ="red";
+        document.querySelector("p").style.fontWeight ="Bold";
+        document.querySelector("p").style.fontSize ="25px";
+        document.querySelector("p").style.opacity ="1";
+        setTimeout(()=>{
+            document.querySelector("p").style.opacity ="0";
+        },4000);
+        
+    }else{
+        li = document.createElement("li"); // create li
+        var input = document.createElement("input");
+        input.type ="checkbox"; // crete checbox
+        input.setAttribute("id","check"); // assigning id to checkbox
+        
+        var label = document.createElement("Label"); // Label created
+        label.setAttribute("for","check"); // assigning for to label
+
+        //Adding above elements to html file/web page -:
+
+        ul.appendChild(li); //ul will hold now li
+        li.appendChild(input); // checkbox is holding the input tag
+        li.appendChild(label); // li will hold label
+        label.appendChild(textNode); // will hold the input from user
+        ul.insertAdjacentElement("afterbegin",li); // now we have to diplay the recent added todo on top
+        //ul.insertBefore(li,ul.children[0]); 
+        setTimeout(()=>{ //For some effects
+            li.className="visual"; //class we cretaed in CSS
+        },250);
+        enteryBar.value =""; // to Clear the input field
+    }
+}
+
+function removeItem(){
+    li = ul.children;
+    for (let index = 0; index < li.length; index++) { // for loop inorder to go through all indexs of li
+        // const element = li[index];  // for cehcking the children and index of li
+        // console.log(element);
+        while (li[index] && li[index].children[0].checked) { //li[index] =  for checking the element && go theough the element index and do a search in children at index 0 and look for checked property
+            ul.removeChild(li[index]) // remove the child of ul i.e li where the checked is found on that index
+        }
+    }
+}
+
+function removeAll(){
+    li = ul.children;
+    for (let index = 0; index < li.length; index++) {
+        while (li[index].children) {
+            ul.removeChild(li[index])
+        }
+    }
+}
